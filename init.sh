483c1ffe6a2329ccf5c16a480399214fd0876939
leonson63 <the_alexis@mail.ru>
2017-01-14 18:59:38 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!bash
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/conf.d/test.conf
 sudo /etc/init.d/nginx restart
